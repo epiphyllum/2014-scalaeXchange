a0ee07a52ce7ac28ee056f38228a2a79f9d419dc
George Leontiev <folone@gmail.com>
2014-12-08 18:50:19 +0100

    Add the naive version for residue.

--- src/main/scala/info.folone.42/Residue.scala
+++ src/main/scala/info.folone.42/Residue.scala
@@ -1,11 +1,20 @@
 package info.folone.fourtytwo
 
 object Residue {
-  case class Residue[N <: Int: SingleInhabitant](n: Long) { lhs =>
-    def +(rhs: Residue[N]): Residue[N] =
-      Residue((lhs.n + rhs.n) % inhabitant[N])
+  object Naive {
+    case class Residue(m: Int, n: Long) { lhs =>
+      def +(rhs: Residue): Residue = if(rhs.m == m) Residue(m, (lhs.n + rhs.n) % m) else throw new RuntimeException
+      def ++(rhs: Residue): Option[Residue] = if(rhs.m == m) Some(Residue(m, (lhs.n + rhs.n) % m)) else None
+    }
   }
+
+  object Typelevel {
+    case class Residue[N <: Int: SingleInhabitant](n: Long) { lhs =>
+      def +(rhs: Residue[N]): Residue[N] =
+        Residue((lhs.n + rhs.n) % inhabitant[N])
+    }
  
-  Residue[15](15) + Residue[15](20)
-  //Residue[13](15) + Residue[13](20)
+    Residue[15](15) + Residue[15](20)
+    //Residue[13](15) + Residue[15](20)
+  }
 }
